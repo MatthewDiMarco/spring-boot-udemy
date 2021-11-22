e4d0d3552ec2245e225cb27cee46d202f0031d36
Matthew Di Marco <mdm@powerledger.io>
2021-11-22 14:46:12 +0800

    Sample spring init project

---
+++ spring-init-test/src/main/java/io/example/springinittest/SpringInitTestApplication.java
@@ -0,0 +1,13 @@
+package io.example.springinittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitTestApplication.class, args);
+    }
+
+}
